b78d77205feeaca52d8cf05bc2aefd283c07c5ba
Guillermo Garcia Perez <ggarciaperez@danielcastelao.org>
2019-10-23 14:08:23 +0200

    Clase

---
+++ coperacao/Clase.java
@@ -0,0 +1,11 @@
+package coperacao;
+
+/**
+ * @author ggarciaperez
+ */
+public class Clase {
+   //Esto es una clase sin sentido
+    public void escribir(){
+        System.out.println("Esto no hace nada");
+    }
+}
